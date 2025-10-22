b65333f734dfe3d59d76da404b9b7c00f3044eb9
aaryanrajputhere <itsaaryanrajput@gmail.com>
2025-10-22 19:55:48 +0530

    React Setup

---
+++ admin/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center">
+      <div>PerkPilot Admin</div>
+    </div>
+  );
+}
+
+export default App;
